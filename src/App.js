38de2ff596658d7aa988b61ff15b74e41b74f6e6
Pratik Chakraborty <pratik@talkinglands.com>
2022-12-12 15:23:42 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +17,13 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <div className='main-box'>
+      <h1>Hello, I am Pratik</h1>
+      <input type="text" />
+      <img className='img-src' src='https://images.unsplash.com/photo-1670826139885-48466e8e21f9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60'/>
+      <button>Submit</button>
+      </div>
     </div>
   );
 }
